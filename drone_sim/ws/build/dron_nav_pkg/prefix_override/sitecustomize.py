f5041255adddf0af9ceb9170ff05cd4ba29a4793
Krzysztof <krzyskuar@gmail.com>
2025-10-04 11:27:13 +0200

    changed gitignore

--- drone_sim/ws/build/dron_nav_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/px4/ros2/ws/install/dron_nav_pkg'
--- drone_sim/ws/build/px4_ros_com/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/px4/ros2/ws/install/px4_msgs/share/px4_msgs/package.sh"
