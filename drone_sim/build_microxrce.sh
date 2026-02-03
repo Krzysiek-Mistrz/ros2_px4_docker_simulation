eb21a18647ebb1da1ced6de3586b83ac0082c188
Krzysiek-Mistrz <krzyskuar@gmail.com>
2026-02-03 13:09:52 +0100

    refactor: added necessary changes 4 RPI ENV (removed sim files and code in docker)

--- drone_sim/build_microxrce.sh
+++ drone_sim/build_microxrce.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
+
+
 set -euo pipefail
+
 echo "Cloning and building Micro-XRCE-DDS-Agent..."
 if [ ! -d ./Micro-XRCE-DDS-Agent ]; then
-  git clone -b v3.0.1 https://github.com/eProsima/Micro-XRCE-DDS-Agent.git ./Micro-XRCE-DDS-Agent
+  rm -r ./Micro-XRCE-DDS-Agent
 fi
-# cd ./Micro-XRCE-DDS-Agent
-# mkdir -p build && cd build
-# cmake ..
-# make -j$(nproc)
-# sudo make install
-# sudo ldconfig
+
+git clone -b v3.0.1 https://github.com/eProsima/Micro-XRCE-DDS-Agent.git ./Micro-XRCE-DDS-Agent
+
+cd ./Micro-XRCE-DDS-Agent
+mkdir -p build && cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
+
 echo "Micro XRCE install done. Binary: ./Micro-XRCE-DDS-Agent/build/MicroXRCEAgent"
