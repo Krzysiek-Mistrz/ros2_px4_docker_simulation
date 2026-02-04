a3bfd09d2f188e4761ec76e0ca82cc328a39f6c1
Krzysiek-Mistrz <krzyskuar@gmail.com>
2026-02-04 15:37:30 +0100

    refactor: deleted submodules since they are installed manually by user at the start, simplified main fly script 4 drone both 4 sim and real env, modified gitignore 2 reflect newest changes 4 sim files

--- drone_sim/build_px4.sh
+++ drone_sim/build_px4.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+
+
 set -euo pipefail
 echo "Cloning and building PX4 (v1.16.0)..."
-if [ ! -d ./PX4-Autopilot ]; then
-  git clone --recursive -b v1.16.0 https://github.com/PX4/PX4-Autopilot.git ./PX4-Autopilot
+
+if [ -d ./PX4-Autopilot ]; then
+  rm -r ./PX4-Autopilot
 fi
+
+git clone --recursive -b v1.16.0 https://github.com/PX4/PX4-Autopilot.git ./PX4-Autopilot
+
 cd ./PX4-Autopilot
 bash ./Tools/setup/ubuntu.sh
 echo "PX4 install done."
+
+# 2 run:
+# make px4_sitl gz_x500
